33e45b108a935d0a1292df13261252f6f70c435a
EmadAishan <openemad@gmail.com>
2022-10-27 10:17:40 +0300

    findOrCreat, {Op}

--- services/authService.js
+++ services/authService.js
@@ -2,18 +2,20 @@
 var jwt = require('jsonwebtoken');
 
 const authService = {
-    // hashPassword: function(plainTextPassword) {
-    //     var salt = bcrypt.genSaltSync(10);
-    //     var hash = bcrypt.hashSync(plainTextPassword, salt);
-    //     return hash
-    // },
-    // comparePassword: function(plain, hash) {
-    //     return bcrypt.compareSync(plain, hash)
-    // },
-    generateToken: function(id, type) {
+    hashPassword: function(plainTextPassword) {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(plainTextPassword, salt);
+        return hash
+    },
+    comparePassword: function(plain, hash) {
+        return bcrypt.compareSync(plain, hash)
+    },
+    generateToken: function(user) {
         return jwt.sign({
-            id: id,
-            type: type
+            id: user.id,
+            type: user?.userTypeId,
+            email: user.email,
+            userName: user.userName
         }, '1234567890')
     },
     decryptToken: function(token) {
