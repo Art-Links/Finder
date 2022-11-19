232f59b4cd74ba451a89cd5b483221872980f708
EmadAishan <openemad@gmail.com>
2022-11-19 10:07:09 +0300

    transformer edit

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
-dotenv.config()
+dotenv.config();
 var indexRouter = require('./routes/index');
 
 var app = express();
+var cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/backend/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use(cors());
 
 app.use(process.env.API_PREFIX, indexRouter);
 
