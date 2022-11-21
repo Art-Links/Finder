2606b580cf9a5092a81bdce863cf7100845af41e
abdulmalekFahd <abdulamlk819@gmail.com>
2022-11-21 20:23:51 +0300

    SomeEdit

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use(cors());
 
 app.use(process.env.API_PREFIX, indexRouter);
